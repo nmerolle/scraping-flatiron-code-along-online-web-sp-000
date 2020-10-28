c8d24414f8fc24353729871f959ce129da082702
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-28 00:16:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,9 +16,9 @@ class Scraper
       #course.description = post.css("p").text
     end
     
-    def get_courses
-  self.get_page.css(".post")
-end
+  def get_courses
+    self.get_page.css(".post")
+  end
  
   def print_courses
     self.make_courses
